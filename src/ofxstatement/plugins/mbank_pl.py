1bb41e76c7e2a7c7a7e08ddd0d7e86189abc89eb
Remy Gwaramadze <remy@gwaramadze.com>
2019-10-26 13:02:47 +0200

    Fix style in mbank_pl module

--- src/ofxstatement/plugins/mbank_pl.py
+++ src/ofxstatement/plugins/mbank_pl.py
@@ -5,7 +5,6 @@ import re
 from ofxstatement import statement
 from ofxstatement.parser import CsvStatementParser
 from ofxstatement.plugin import Plugin
-from ofxstatement.statement import Statement
 
 
 class MBankPLPlugin(Plugin):
@@ -21,45 +20,47 @@ class MBankPLPlugin(Plugin):
 
 
 class MBankPLParser(CsvStatementParser):
-    mappings = {"date_user":0,
-                "date": 1,
-                "amount": 6, }
-    
+    mappings = {
+        "date_user": 0,
+        "date": 1,
+        "amount": 6,
+    }
+
     date_format = "%Y-%m-%d"
-    
+
     def parse(self):
         self.parsing_header = True
         self.last_line = None
         return super(MBankPLParser, self).parse()
-    
+
     def split_records(self):
         return csv.reader(self.fin, delimiter=';', quotechar='"')
-    
+
     def parse_record(self, line):
-        
+
         if self.parsing_header:
             return self.parse_header(line)
-        
+
         if len(line) != 9:
             return None
-        
+
         # footer
         if line[6] == "#Saldo końcowe":
             self.statement.end_balance = self.parse_float(line[7])
             return None
-        
+
         sl = super(MBankPLParser, self).parse_record(line)
         sl.date_user = datetime.strptime(sl.date_user, self.date_format)
 
         # account number and name
         sl.memo = re.sub("\s+", " ", ' '.join([line[5].strip("'"), line[4]]))
-        
+
         # type - description
         sl.payee = re.sub("\s+", " ", ' - '.join(line[2:4]))
 
         # generate transaction id out of available data
         sl.id = statement.generate_transaction_id(sl)
-        
+
         if line[2].startswith("PRZ"):
             sl.trntype = "XFER"
         elif line[2].startswith("WYPŁATA"):
@@ -74,15 +75,16 @@ class MBankPLParser(CsvStatementParser):
             sl.trntype = "INT"
 
         return sl
-    
+
     def parse_float(self, value):
-        return super(MBankPLParser, self).parse_float(re.sub("[ .a-zA-Z]", "", value).replace(",", "."))
-    
+        return super(MBankPLParser, self).parse_float(
+            re.sub("[ .a-zA-Z]", "", value).replace(",", "."))
+
     def parse_header(self, line):
-        
+
         stmt = self.statement
         last = self.last_line
-        
+
         if not line:
             pass
         elif line[0] == "Łącznie":
@@ -103,5 +105,5 @@ class MBankPLParser(CsvStatementParser):
             stmt.account_id = "PL" + line[0].replace(" ", "")
 
         self.last_line = line
-        
+
         return None
